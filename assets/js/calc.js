34c4a2677bf1d2924a8ca6c5ecf234295459e5fa
Farah M <farah.maria.rahman@gmail.com>
2022-10-04 20:10:51 +0000

    add first passing test

---
+++ assets/js/calc.js
@@ -0,0 +1,5 @@
+function addition() {
+    return 42;
+}
+
+module.exports = addition; // this exports the function so it can be accessed in the test file
---
+++ assets/js/tests/calc.test.js
@@ -0,0 +1,11 @@
+const { expect } = require("expect");
+
+const addition = (require("../calc"));
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20,22)).toBe(42);
+      });
+    });
+});
