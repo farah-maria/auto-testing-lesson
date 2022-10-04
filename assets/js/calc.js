51c6baf33291d63be1cbc0341e5d3fdf5cf0a020
Farah M <farah.maria.rahman@gmail.com>
2022-10-04 20:38:50 +0000

    add extra test

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,5 +1,5 @@
-function addition() {
-    return 42;
+function addition(num1, num2) {
+    return num1 + num2; 
 }
 
 module.exports = addition; // this exports the function so it can be accessed in the test file
