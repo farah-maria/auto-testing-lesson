6b8860cf9baaf87e05734e0baeba3e794fbee01d
Farah M <farah.maria.rahman@gmail.com>
2022-10-06 15:56:37 +0000

    add extra tests

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,5 +1,24 @@
-function addition() {
+/* function addition() {
     return 42;
+}
+
+module.exports = addition; */
+
+
+/* NEXT PHASE
+
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+    }); */
+
+
+function addition(num1, num2) {
+    return num1 + num2;
 }
 
 module.exports = addition;
